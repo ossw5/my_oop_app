c67fbdbd21ab2a251a87652b5ad9901272217944
Robert Romero <oswromero521@gmail.com>
2025-08-10 19:12:48 +0800

    feat: add trecia widget

--- oop_classes/lib/group.dart
+++ oop_classes/lib/group.dart
@@ -2,7 +2,7 @@ class Group {
   String name;
   int age;
   Group(this.name, this.age);
-  void sayHello() {
+  void describe() {
     print('Hello! I am $name and I am $age years old.');
   }
 }
--- oop_classes/lib/main.dart
+++ oop_classes/lib/main.dart
@@ -2,11 +2,11 @@ import 'group.dart';
 
 void main() {
   var p1 = Group('Trecia', 20);
-  p1.sayHello();
+  p1.describe();
   var p2 = Group('Robert', 20);
-  p2.sayHello();
+  p2.describe();
   var p3 = Group('Jaypee', 19);
-  p3.sayHello();
+  p3.describe();
   var p4 = Group('Matt Vencey', 20);
-  p4.sayHello();
+  p4.describe();
 }
